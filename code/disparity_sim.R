8225d50a4bfbedf1a4a57d97771377a7c83fb4c9
Catherine Klein <catherine.g.klein@gmail.com>
2022-09-08 14:53:04 +0200

    Adding more metrics

--- code/disparity_sim.R
+++ code/disparity_sim.R
@@ -1,6 +1,6 @@
 
 
-setwd("~/Desktop/DisparityBias/code/")
+setwd("~/Desktop/disparity/DisparityBias/code/")
 
 source("functions_DisaBiss.R")
 
@@ -33,38 +33,95 @@ bins <- 3 # number of time bins
 fossil.colour1 <- "#FF6EB4"
 fossil.colour2 <- "#C0FF3E"
 
-num.rep <- 8
+num.rep <- 3
 
 #Running the simulations
 simulations <- replicate(num.rep, simulation.pipeline(birth, death, tips, trait.num, trait.evol.rate, fossilisation.rate, migration.rate, fossils.in.area1, threshold, iteration.limit, low.sampling, high.sampling, bins, fossil.colour1, fossil.colour2), simplify = FALSE)
 
 
 
-#### Analysis
+#### Analysis -- Sum of variances
 #Measure the disparity on the output using lapply (applying a function to a list)
 sum.variances <- lapply(simulations, dispRity, metric = c(sum, variances))
 
 #Extract the disparity values (the point estimates explained above)
-point.estimates <- lapply(sum.variances, extract.dispRity)
+point.estimates.sumv <- lapply(sum.variances, extract.dispRity)
 
 ##Combine that into a more reader friendly format (a table!) using the rbind function (bind in rows) applied to this list of lists using do.call
-results.table <- do.call(rbind, point.estimates)
+results.table.sumv <- do.call(rbind, point.estimates.sumv)
 
 columns <- c("values", "sampling")
-result <- data.frame(matrix(nrow = 0, ncol = length(columns)))
-colnames(result) <- columns
+result.sumv <- data.frame(matrix(nrow = 0, ncol = length(columns)))
+colnames(result.sumv) <- columns
 
-for (i in 1:ncol(results.table)){
-  for (j in results.table[,i]){
-    current.row = c(j,colnames(results.table)[i])
-    result[nrow(result) + 1,] <- current.row
+for (i in 1:ncol(results.table.sumv)){
+  for (j in results.table.sumv[,i]){
+    current.row = c(j,colnames(results.table.sumv)[i])
+    result.sumv[nrow(result.sumv) + 1,] <- current.row
   }
 }
 
-result$sampling <- as.factor(result$sampling)
-result$values <- as.numeric(result$values)
-p <- ggplot(result, aes(x=sampling, y=values)) +
+result.sumv$sampling <- as.factor(result.sumv$sampling)
+result.sumv$values <- as.numeric(result.sumv$values)
+p <- ggplot(result.sumv, aes(x=sampling, y=values)) +
   geom_boxplot()
 p
 
 
+
+
+#### Analysis -- MEdian pairwise distances
+#Measure the disparity on the output using lapply (applying a function to a list)
+med.pair.distances <- lapply(simulations, dispRity, metric = c(median, pairwise.dist))
+
+#Extract the disparity values (the point estimates explained above)
+point.estimates.mpd <- lapply(med.pair.distances, extract.dispRity)
+
+##Combine that into a more reader friendly format (a table!) using the rbind function (bind in rows) applied to this list of lists using do.call
+results.table.mpd <- do.call(rbind, point.estimates.mpd)
+
+columns <- c("values", "sampling")
+result.mpd <- data.frame(matrix(nrow = 0, ncol = length(columns)))
+colnames(result.mpd) <- columns
+
+for (i in 1:ncol(results.table.mpd)){
+  for (j in results.table.mpd[,i]){
+    current.row = c(j,colnames(results.table.mpd)[i])
+    result.mpd[nrow(result.mpd) + 1,] <- current.row
+  }
+}
+
+result.mpd$sampling <- as.factor(result.mpd$sampling)
+result.mpd$values <- as.numeric(result.mpd$values)
+q <- ggplot(result.mpd, aes(x=sampling, y=values)) +
+  geom_boxplot()
+q
+
+
+
+#### Analysis -- Median distance from centroids
+#Measure the disparity on the output using lapply (applying a function to a list)
+med.centroid.distances <- lapply(simulations, dispRity, metric = c(median, centroids))
+
+#Extract the disparity values (the point estimates explained above)
+point.estimates.mcd <- lapply(med.centroid.distances, extract.dispRity)
+
+##Combine that into a more reader friendly format (a table!) using the rbind function (bind in rows) applied to this list of lists using do.call
+results.table.mcd <- do.call(rbind, point.estimates.mcd)
+
+columns <- c("values", "sampling")
+result.mcd <- data.frame(matrix(nrow = 0, ncol = length(columns)))
+colnames(result.mcd) <- columns
+
+for (i in 1:ncol(results.table.mcd)){
+  for (j in results.table.mcd[,i]){
+    current.row = c(j,colnames(results.table.mcd)[i])
+    result.mcd[nrow(result.mcd) + 1,] <- current.row
+  }
+}
+
+result.mcd$sampling <- as.factor(result.mcd$sampling)
+result.mcd$values <- as.numeric(result.mcd$values)
+c <- ggplot(result.mcd, aes(x=sampling, y=values)) +
+  geom_boxplot()
+c
